cf82c25c5389286b209781a615a34612996bb3e3
danbonilla <dbonilla11@gmail.com>
2015-03-11 14:02:19 -0400

    updated query_set for portfolioHistory

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from api.models import Portfolio, PortfolioHistory, BenchmarkHistory
 from .serializers import PortfolioHistorySerializer
+from .serializers import BenchmarkHistorySerializer
 
 
 class PortfolioView(generics.ListAPIView):
@@ -10,6 +11,28 @@ class PortfolioView(generics.ListAPIView):
         portfolio_id = self.kwargs['id']
         fromdate = self.request.QUERY_PARAMS.get('fromdate', None)
         todate = self.request.QUERY_PARAMS.get('todate', None)
+        if fromdate and todate: 
 
-    	query_set = PortfolioHistory.objects.filter(portfolio_id=portfolio_id).filter(date__range=[fromdate, todate]).order_by('-date')  	
-        return query_set
+          query_set = PortfolioHistory.objects.filter(portfolio_id=portfolio_id).filter(date__range=[fromdate, todate]).order_by('-date')  
+
+        elif fromdate: 
+          query_set = PortfolioHistory.objects.filter(portfolio_id=portfolio_id).filter(date__gte=fromdate).order_by('-date')
+
+        elif todate: 
+          query_set = PortfolioHistory.objects.filter(portfolio_id=portfolio_id).filter(date__lte=todate).order_by('-date')
+
+        else: 
+          query_set = PortfolioHistory.objects.filter(portfolio_id=portfolio_id).order_by('-date')
+
+        return query_set 
+
+class BenchmarkHistoryView(generics.ListAPIView):
+    serializer_class = BenchmarkHistorySerializer
+
+    def get_queryset(self):
+        fromdate = self.request.QUERY_PARAMS.get('fromdate', None)
+        todate = self.request.QUERY_PARAMS.get('todate', None)
+        if fromdate and todate: 
+
+          query_set = BenchmarkHistory.objects.filter(portfolio_id=portfolio_id).filter(date__range=[fromdate, todate]).order_by('-date')  
+
